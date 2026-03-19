30d2407241703fce1acc6eadcb9f33811ce184dd
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2026-03-19 16:37:13 -0700

    Added Supabase folder with server/client files.

---
+++ lib/supabase/server.ts
@@ -0,0 +1,25 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export async function createServerSupabaseClient() {
+	const cookieStore = await cookies();
+
+	return createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			cookies: {
+				getAll() {
+                    return cookieStore.getAll();
+                    // getAll() - reads all cookies (So Supabase can find my session)
+				},
+                setAll(cookiesToSet) {
+                    // setAll() - writes cookies back(so your session stays fresh)
+					cookiesToSet.forEach(({ name, value, options }) => {
+						cookieStore.set(name, value, options);
+					});
+				},
+			},
+		},
+	);
+}
