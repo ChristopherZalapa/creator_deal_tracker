e276d364ce9d329e194730c3a665b08d8c5afc02
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2026-03-24 08:22:19 -0700

    Created creators folder and pages

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -12,5 +12,39 @@ export default async function Dashboard() {
 		redirect("/login");
 	}
 
-	return <h1>This is the Dashboard</h1>;
+	const { data: profile } = await supabase
+		.from("profiles")
+		.select("*")
+		.eq("id", user!.id)
+		.single();
+
+	return (
+		<div className='p-8'>
+			<h1 className='text-white text-2xl font-semibold mb-8'>
+				Welcome Back, {profile?.full_name}
+			</h1>
+
+			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+				<div className='border bg-white/5 border-white/10 rounded-xl p-6'>
+					<p className='text-zinc-400 text-sm mb-2'>Total Creators</p>
+					<h2 className='text-white text-2xl font-bold'>24</h2>
+				</div>
+
+				<div className='border bg-white/5 border-white/10 rounded-xl p-6'>
+					<p className='text-zinc-400 text-sm mb-2'>Active Deals</p>
+					<h2 className='text-white text-2xl font-bold'>24</h2>
+				</div>
+
+				<div className='border bg-white/5 border-white/10 rounded-xl p-6'>
+					<p className='text-zinc-400 text-sm mb-2'>Total Revenue</p>
+					<h2 className='text-white text-2xl font-bold'>24</h2>
+				</div>
+
+				<div className='border bg-white/5 border-white/10 rounded-xl p-6'>
+					<p className='text-zinc-400 text-sm mb-2'>Conversion Rate</p>
+					<h2 className='text-white text-2xl font-bold'>24</h2>
+				</div>
+			</div>
+		</div>
+	);
 }
