ca49a3e62645d7e6ff5e3db52c7eb7d398a54fb4
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2026-04-18 19:42:29 -0700

    Added updateDealsStatus action

--- app/(protected)/deals/actions.tsx
+++ app/(protected)/deals/actions.tsx
@@ -27,3 +27,10 @@ export async function updateDeal(
 
 	revalidatePath("/deals");
 }
+
+export async function updateDealsStatus(id: string, status: string) {
+	const supabase = await createClient();
+	await supabase.from("deals").update({ status }).eq("id", id);
+
+	revalidatePath("/kanban");
+}
