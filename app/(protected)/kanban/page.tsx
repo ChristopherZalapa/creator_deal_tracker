888b84c627a28ece5c9814a70bc77723528c9bc4
Christopher Zalapa <63930010+ChristopherZalapa@users.noreply.github.com>
2026-04-18 19:05:07 -0700

    Added kanban placeholder page

---
+++ app/(protected)/kanban/page.tsx
@@ -0,0 +1,8 @@
+export default function KanbanPage() {
+	return (
+		<div className='p-8'>
+			<h1 className='text-white text-2xl font-semibold mb-8'>Kanban</h1>
+			<p className='text-zinc-400'>Coming soon...</p>
+		</div>
+	);
+}
